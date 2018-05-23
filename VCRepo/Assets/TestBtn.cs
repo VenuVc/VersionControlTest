b523cb5963351356917348227ea3d8850c7cdc0d
VenuVc <36507019+VenuVc@users.noreply.github.com>
2018-05-23 12:15:25 +0530

    Update Canves and Particle system

---
+++ VCRepo/Assets/TestBtn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBtn : MonoBehaviour {
+
+
+    public GameObject obj1;
+    public GameObject obj2;
+    // Use this for initialization
+    void Start () {
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickMe() {
+
+        obj1.SetActive(true);
+        obj2.SetActive(true);
+
+    }
+}
